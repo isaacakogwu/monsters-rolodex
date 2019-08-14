a4c9ce65f273be090408cf0277767a4f60dded3e
Isaac Akogwu <isaacakogwu11@gmail.com>
2019-08-14 11:46:10 +0100

    Updates

---
+++ precache-manifest.f0f0188642cf1ccfd8a259cb06356e47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c2478e822d7319f758b3fac846e287",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0e50e39b6f2eabd83a45",
+    "url": "/monsters-rolodex/static/css/main.f05b76dd.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "0e50e39b6f2eabd83a45",
+    "url": "/monsters-rolodex/static/js/main.334a9c76.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
